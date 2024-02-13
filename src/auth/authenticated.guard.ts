db15b33fa45ebbfcb57d9dd412066c2fe12061dc
TaekyungHa <86tkstar@gmail.com>
2024-02-13 16:01:00 +0900

    session기능 구현

---
+++ src/auth/authenticated.guard.ts
@@ -0,0 +1,8 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+
+export class AuthenticatedGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    return req.isAuthenticated();
+  }
+}
---
+++ src/utils/session.util.ts
@@ -0,0 +1,3 @@
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+
+export const setSession = async (app: NestFastifyApplication) => {};
